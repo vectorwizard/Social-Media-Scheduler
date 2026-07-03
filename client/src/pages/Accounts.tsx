36a2eea8a2310a2ace2479ab90be4ae684a9d078
Arijit <arijitroylochan@gmail.com>
2026-07-04 00:05:56 +0530

    Accounts page added

--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { dummyAccountsData, PLATFORMS } from "../assets/assets"
+import { PlusIcon } from "lucide-react"
+import AccountList from "../components/AccountList"
+import PlatformPickerModal from "../components/PlatformPickerModal"
 
 const Accounts = () => {
+
+  const [accounts, setAccounts] = useState<any[]>([])
+  const [connecting, setConnecting] = useState<string | null>(null)
+  const [showPlatformPicker, setShowPlatformPicker] = useState(false)
+
+  const fetchAccounts = async (isSync = false, platform?: string | null, successMsg?:string)=>{
+    setAccounts(dummyAccountsData);
+    console.log(isSync,platform, successMsg)
+  }
+
+  useEffect(()=>{
+    fetchAccounts();
+  },[])
+
+  const handleConnect = async (platformId:string) => {
+    setConnecting(platformId);
+    setTimeout(()=>{
+      setConnecting(null)
+      setAccounts((prev)=>[...prev, dummyAccountsData[0]])
+      setShowPlatformPicker(false);
+    })
+  }
+
+  const handleDisconnect = async (accountId:string) => {
+    setAccounts(accounts.filter((a)=>a._id !== accountId))
+  }
+
+  const connectedIds = accounts.map((a)=> a.platform)
+
   return (
-    <div>
-      Accounts
+    <div className="space-y-8 max-w-4xl">
+      {/* Header */}
+      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 text-sm">
+        <div>
+          <h2 className="text-xl text-slate-900">Connected Accounts</h2>
+          <p className="text-slate-500 text-sm mt-0.5">{accounts.length} of {PLATFORMS.length} platforms connected</p>
+        </div>
+        <button onClick={()=> setShowPlatformPicker(true)} className="flex items-center gap-2 px-5 py-2.5 bg-red-500 hover:bg-red-600 text-white rounded-full font-medium transition-all w-full sm:w-auto justify-center">
+          <PlusIcon className="size-4" /> Connect Account
+        </button>
+      </div>
+
+      {/* Platform pickar modal */}
+      {showPlatformPicker && <PlatformPickerModal connectedIds={connectedIds} connecting={connecting} onClose={()=>setShowPlatformPicker(false)} onConnect={handleConnect}/>}
+
+      {/* Connected accounts list */}
+      <AccountList accounts={accounts} onDisconnect={handleDisconnect}/>
+
     </div>
   )
 }
